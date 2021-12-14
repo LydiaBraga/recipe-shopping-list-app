6e530b1e33e0111afadf666dff5586043754f240
Lydia Braga <lydia.braga@inatel.br>
2021-12-14 12:05:41 -0300

    Creating components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "This is a test",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg"
+    ),
+    new Recipe(
+      "Test Recipe",
+      "This is a test",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg"
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
